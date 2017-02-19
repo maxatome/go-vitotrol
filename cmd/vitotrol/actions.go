d6115e8606a90a413265d9f08c6500c1aa72a7e5
Maxime Soulé <btik-git@scoubidou.com>
2017-02-19 15:47:36 +0100

    Unbloat cmd/vitotrol/main.go by introducing actions

---
+++ cmd/vitotrol/actions.go
@@ -0,0 +1,352 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/maxatome/go-vitotrol"
+	"io/ioutil"
+	"strings"
+)
+
+func checkAttributeAccess(attrName string, reqAccess vitotrol.AttrAccess) (vitotrol.AttrId, error) {
+	attrId, ok := vitotrol.AttributesNames2Ids[attrName]
+	if !ok {
+		return vitotrol.NoAttr, fmt.Errorf("unknown attribute `%s'", attrName)
+	}
+
+	if (vitotrol.AttributesRef[attrId].Access & reqAccess) != reqAccess {
+		return vitotrol.NoAttr, fmt.Errorf("attribute `%s' is not %s",
+			attrName, vitotrol.AccessToStr[reqAccess])
+	}
+
+	return attrId, nil
+}
+
+func existTimesheetName(tsName string) (vitotrol.TimesheetId, error) {
+	tId, ok := vitotrol.TimesheetsNames2Ids[tsName]
+	if !ok {
+		return 0, fmt.Errorf("unknown timesheet `%s'", tsName)
+	}
+	return tId, nil
+}
+
+type Action interface {
+	NeedAuth() bool
+	Do(pOptions *Options, params []string) error
+}
+
+var actions = map[string]Action{
+	"list":          &ListAction{},
+	"get":           &GetAction{},
+	"rget":          &GetAction{rget: true},
+	"set":           &SetAction{},
+	"errors":        &ErrorsAction{},
+	"timesheet":     &TimesheetAction{},
+	"set_timesheet": &SetTimesheetAction{},
+}
+
+type authAction struct {
+	v       *vitotrol.Session
+	d       *vitotrol.Device
+	options *Options
+}
+
+func (a *authAction) initVitotrol(pOptions *Options) error {
+	v := &vitotrol.Session{
+		Debug: pOptions.debug,
+	}
+
+	err := v.Login(pOptions.login, pOptions.password)
+	if err != nil {
+		return fmt.Errorf("Login failed: %s", err)
+	}
+
+	err = v.GetDevices()
+	if err != nil {
+		return fmt.Errorf("GetDevices failed: %s", err)
+	}
+	if len(v.Devices) == 0 {
+		return errors.New("No device found!")
+	}
+
+	pDevice := &v.Devices[0]
+	if !pDevice.IsConnected {
+		return fmt.Errorf("Device %s@%s is not connected",
+			pDevice.DeviceName, pDevice.LocationName)
+	}
+
+	if pOptions.verbose {
+		fmt.Printf("Working with device %s@%s\n",
+			pDevice.DeviceName, pDevice.LocationName)
+	}
+
+	a.v = v
+	a.d = pDevice
+	a.options = pOptions
+
+	return nil
+}
+
+func (a *authAction) NeedAuth() bool {
+	return true
+}
+
+// ListAction implements the "list" action.
+type ListAction struct{}
+
+func (a *ListAction) NeedAuth() bool {
+	return false
+}
+
+func (a *ListAction) Do(pOptions *Options, params []string) error {
+	if len(params) == 0 || params[0] == "attrs" {
+		for _, pAttrRef := range vitotrol.AttributesRef {
+			fmt.Println(pAttrRef)
+		}
+		return nil
+	}
+
+	if params[0] == "timesheets" {
+		for _, pTimesheetRef := range vitotrol.TimesheetsRef {
+			fmt.Println(pTimesheetRef)
+		}
+		return nil
+	}
+
+	return fmt.Errorf(
+		"`list' action allows `attrs' or `timesheets' params, not `%s'",
+		params[0])
+}
+
+// GetAction implements the "get" and "rget" actions.
+type GetAction struct {
+	authAction
+	rget bool
+}
+
+func (a *GetAction) Do(pOptions *Options, params []string) error {
+	if len(params) == 0 {
+		return errors.New("at least one PARAM is missing!")
+	}
+
+	var attrs []vitotrol.AttrId
+	// Special case -> all attributes
+	if len(params) == 1 && params[0] == "all" {
+		attrs = vitotrol.Attributes
+	} else {
+		attrs = make([]vitotrol.AttrId, len(params))
+		var err error
+		for idx, attrName := range params {
+			attrs[idx], err = checkAttributeAccess(attrName, vitotrol.ReadOnly)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	err := a.initVitotrol(pOptions)
+	if err != nil {
+		return err
+	}
+
+	if a.rget {
+		ch, err := a.d.RefreshDataWait(a.v, attrs)
+		if err != nil {
+			return fmt.Errorf("RefreshData error: %s", err)
+		}
+
+		if err = <-ch; err != nil {
+			return fmt.Errorf("RefreshData failed: %s", err)
+		}
+	}
+
+	err = a.d.GetData(a.v, attrs)
+	if err != nil {
+		return fmt.Errorf("GetData error: %s", err)
+	}
+
+	fmt.Print(a.d.FormatAttributes(attrs))
+	return nil
+}
+
+// SetAction implements the "set" action.
+type SetAction struct {
+	authAction
+}
+
+func (a *SetAction) Do(pOptions *Options, params []string) error {
+	if len(params) == 0 || (len(params)&1) != 0 {
+		return errors.New("PARAMS must be a list of pairs: ATTR_NAME VALUE ...")
+	}
+
+	attrsValues := make(map[vitotrol.AttrId]string, len(params)/2)
+	for idx := 0; idx < len(params); idx += 2 {
+		attrId, err := checkAttributeAccess(params[idx], vitotrol.WriteOnly)
+		if err != nil {
+			return err
+		}
+
+		value, err :=
+			vitotrol.AttributesRef[attrId].Type.Human2VitodataValue(params[idx+1])
+		if err != nil {
+			return fmt.Errorf("value `%s' of attribute %s is invalid: %s",
+				params[idx+1], params[idx], err)
+		}
+
+		attrsValues[attrId] = value
+	}
+
+	err := a.initVitotrol(pOptions)
+	if err != nil {
+		return err
+	}
+
+	// Set them all
+	for attrId, value := range attrsValues {
+		ch, err := a.d.WriteDataWait(a.v, attrId, value)
+		if err != nil {
+			return fmt.Errorf("WriteData error: %s", err)
+		}
+
+		if err = <-ch; err != nil {
+			return fmt.Errorf("WriteData failed: %s", err)
+		}
+
+		if pOptions.verbose {
+			fmt.Printf("%s attribute successfully set to `%s'\n",
+				vitotrol.AttributesRef[attrId].Name, value)
+		}
+	}
+
+	return nil
+}
+
+// ErrorsAction implements the "errors" action.
+type ErrorsAction struct {
+	authAction
+}
+
+func (a *ErrorsAction) Do(pOptions *Options, params []string) error {
+	err := a.initVitotrol(pOptions)
+	if err != nil {
+		return err
+	}
+
+	err = a.d.GetErrorHistory(a.v)
+	if err != nil {
+		return fmt.Errorf("GetErrorHistory error: %s", err)
+	}
+
+	if len(a.d.Errors) == 0 {
+		fmt.Println("No errors")
+	} else {
+		fmt.Printf("%d error(s):\n", len(a.d.Errors))
+		for _, error := range a.d.Errors {
+			fmt.Println("-", &error)
+		}
+	}
+
+	return nil
+}
+
+// SetTimesheetAction implements the "set_timesheet" action.
+type SetTimesheetAction struct {
+	authAction
+}
+
+func (a *SetTimesheetAction) Do(pOptions *Options, params []string) error {
+	if len(params) == 0 {
+		return errors.New("timesheet name is missing!")
+	}
+
+	var err error
+
+	tId, err := existTimesheetName(params[0])
+	if err != nil {
+		return err
+	}
+
+	if len(params) == 1 {
+		return errors.New("JSON definition of timesheet is missing!")
+	}
+	tss := make(map[string]vitotrol.TimeslotSlice)
+	var data []byte
+	if strings.HasPrefix(params[1], "@") && len(params[1]) > 1 {
+		data, err = ioutil.ReadFile(params[1][1:])
+		if err != nil {
+			return fmt.Errorf("Cannot read file %s: %s", params[1][1:], err)
+		}
+	} else {
+		data = []byte(params[1])
+	}
+	err = json.Unmarshal(data, &tss)
+	if err != nil {
+		return fmt.Errorf("JSON definition of timesheet is invalid: %s", err)
+	}
+
+	err = a.initVitotrol(pOptions)
+	if err != nil {
+		return err
+	}
+
+	ch, err := a.d.WriteTimesheetDataWait(a.v, tId, tss)
+	if err != nil {
+		return fmt.Errorf("WriteTimesheetData error: %s", err)
+	}
+
+	if err = <-ch; err != nil {
+		return fmt.Errorf("WriteTimesheetData failed: %s", err)
+	}
+
+	return nil
+}
+
+// TimesheetAction implements the "timesheet" action.
+type TimesheetAction struct {
+	authAction
+}
+
+func (a *TimesheetAction) Do(pOptions *Options, params []string) error {
+	if len(params) == 0 {
+		return errors.New("timesheet name is missing!")
+	}
+
+	var err error
+
+	timesheetIds := make([]vitotrol.TimesheetId, len(params))
+	for idx, name := range params {
+		timesheetIds[idx], err = existTimesheetName(name)
+		if err != nil {
+			return err
+		}
+	}
+
+	err = a.initVitotrol(pOptions)
+	if err != nil {
+		return err
+	}
+
+	for _, tId := range timesheetIds {
+		err := a.d.GetTimesheetData(a.v, tId)
+		if err != nil {
+			return fmt.Errorf("GetTimesheetData error: %s", err)
+		}
+
+		ts := a.d.Timesheets[tId]
+		if a.options.jsonOutput {
+			buf, _ := json.Marshal(ts)
+			fmt.Println(string(buf))
+		} else {
+			fmt.Println(vitotrol.TimesheetsRef[tId])
+			for _, day := range []string{"mon", "tue", "wed", "thu", "fri", "sat", "sun"} {
+				fmt.Printf("- %s:\n", day)
+				for _, slot := range ts[day] {
+					fmt.Printf("  %s\n", &slot)
+				}
+			}
+		}
+	}
+
+	return nil
+}
--- cmd/vitotrol/main.go
+++ cmd/vitotrol/main.go
@@ -2,15 +2,10 @@ package main
 
 import (
 	"bufio"
-	"encoding/json"
-	"errors"
 	"flag"
 	"fmt"
-	"github.com/maxatome/go-vitotrol"
-	"io/ioutil"
 	"os"
 	"path"
-	"strings"
 )
 
 var allowedActions = map[string]bool{
@@ -21,6 +16,14 @@ var allowedActions = map[string]bool{
 	"set":           true,
 	"timesheet":     true,
 	"set_timesheet": true,
+}
+
+type Options struct {
+	login      string
+	password   string
+	verbose    bool
+	debug      bool
+	jsonOutput bool
 }
 
 func main() {
@@ -45,14 +48,14 @@ ACTION & PARAMS can be:
 - errors               get the error history`)
 	}
 
-	var login, password, config string
-	var verbose, debug, jsonOutput bool
-	flag.StringVar(&login, "login", "", "login on vitotrol API")
-	flag.StringVar(&password, "password", "", "password on vitotrol API")
+	var options Options
+	var config string
+	flag.StringVar(&options.login, "login", "", "login on vitotrol API")
+	flag.StringVar(&options.password, "password", "", "password on vitotrol API")
 	flag.StringVar(&config, "config", "", "login+password config file")
-	flag.BoolVar(&verbose, "verbose", false, "print verbose information")
-	flag.BoolVar(&debug, "debug", false, "print debug information")
-	flag.BoolVar(&jsonOutput, "json", false,
+	flag.BoolVar(&options.verbose, "verbose", false, "print verbose information")
+	flag.BoolVar(&options.debug, "debug", false, "print debug information")
+	flag.BoolVar(&options.jsonOutput, "json", false,
 		"used by `timesheet' action to display timesheets using JSON format")
 
 	flag.Parse()
@@ -62,327 +65,66 @@ ACTION & PARAMS can be:
 		os.Exit(1)
 	}
 
-	action, params := flag.Args()[0], flag.Args()[1:]
+	actionName, params := flag.Args()[0], flag.Args()[1:]
 
-	if !allowedActions[action] {
+	action := actions[actionName]
+	if action == nil {
 		fmt.Fprintf(os.Stderr, "*** bad action `%s'\n", action)
 		flag.Usage()
 		os.Exit(1)
 	}
 
-	// This action dos not need auth
-	if action == "list" {
-		if len(params) == 0 || params[0] == "attrs" {
-			for _, pAttrRef := range vitotrol.AttributesRef {
-				fmt.Println(pAttrRef)
+	var err error
+	if action.NeedAuth() {
+		// Load config if login OR password is missing
+		if options.login == "" || options.password == "" {
+			if config == "" {
+				config = path.Join(os.Getenv("HOME"), ".vitotrol-api")
 			}
-		} else if params[0] == "timesheets" {
-			for _, pTimesheetRef := range vitotrol.TimesheetsRef {
-				fmt.Println(pTimesheetRef)
-			}
-		} else {
-			fmt.Fprintf(os.Stderr,
-				"`list' action allows `attrs' or `timesheets' params, not `%s'\n",
-				params[0])
-			flag.Usage()
-			os.Exit(1)
-		}
-		return
-	}
 
-	// Load config if login OR password is missing
-	if login == "" || password == "" {
-		if config == "" {
-			config = path.Join(os.Getenv("HOME"), ".vitotrol-api")
-		}
-
-		file, err := os.Open(config)
-		if err != nil {
-			fmt.Fprintf(os.Stderr,
-				"--login & --password are mandatory "+
-					"EXCEPT if `%s' file exists and is readdable\n",
-				config)
-			os.Exit(1)
-		}
-
-		info, err := file.Stat()
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Cannot stat `%s' file: %s\n", config, err)
-			os.Exit(1)
-		}
-
-		if (info.Mode() & 06) != 0 {
-			fmt.Fprintf(os.Stderr,
-				"`%s' file readdable and/or writtable by others. Abort!\n", config)
-			os.Exit(1)
-		}
-
-		rd := bufio.NewReader(file)
-		confLogin, _ := rd.ReadString('\n')
-		confPassword, err := rd.ReadString('\n')
-		file.Close()
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Invalid config file `%s' contents, must contain "+
-				"login and password on two separate lines\n", config)
-			os.Exit(1)
-		}
-
-		if login == "" {
-			login = confLogin[:len(confLogin)-1]
-		}
-		if password == "" {
-			password = confPassword[:len(confPassword)-1]
-		}
-	}
-
-	if action != "errors" && len(params) == 0 {
-		fmt.Fprintln(os.Stderr, "*** at least one PARAM is missing!")
-		flag.Usage()
-		os.Exit(1)
-	}
-
-	switch action {
-	case "get", "rget":
-		var attrs []vitotrol.AttrId
-		// Special case -> all attributes
-		if len(params) == 1 && params[0] == "all" {
-			attrs = vitotrol.Attributes
-		} else {
-			attrs = make([]vitotrol.AttrId, len(params))
-			for idx, attrName := range params {
-				attrs[idx] = mustCheckAttributeAccess(attrName, vitotrol.ReadOnly)
-			}
-		}
-
-		v, pDevice := mustInitVitotrol(login, password, verbose, debug)
-
-		if action == "rget" {
-			ch, err := pDevice.RefreshDataWait(v, attrs)
+			file, err := os.Open(config)
 			if err != nil {
-				fmt.Fprintln(os.Stderr, "RefreshData error:", err)
+				fmt.Fprintf(os.Stderr,
+					"--login & --password are mandatory "+
+						"EXCEPT if `%s' file exists and is readdable\n",
+					config)
 				os.Exit(1)
 			}
 
-			if err = <-ch; err != nil {
-				fmt.Fprintln(os.Stderr, "RefreshData failed:", err)
-				os.Exit(1)
-			}
-		}
-
-		err := pDevice.GetData(v, attrs)
-		if err != nil {
-			fmt.Fprintln(os.Stderr, "GetData error:", err)
-			os.Exit(1)
-		}
-
-		fmt.Print(pDevice.FormatAttributes(attrs))
-
-	case "set":
-		if (len(params) & 1) != 0 {
-			fmt.Fprintln(os.Stderr,
-				"*** PARAMS must be a list of pairs: ATTR_NAME VALUE ...")
-			flag.Usage()
-			os.Exit(1)
-		}
-		attrsValues := make(map[vitotrol.AttrId]string, len(params)/2)
-		for idx := 0; idx < len(params); idx += 2 {
-			attrId := mustCheckAttributeAccess(params[idx], vitotrol.WriteOnly)
-
-			value, err :=
-				vitotrol.AttributesRef[attrId].Type.Human2VitodataValue(params[idx+1])
+			info, err := file.Stat()
 			if err != nil {
-				fmt.Fprintf(os.Stderr, "*** value `%s' of attribute %s is invalid: %s\n",
-					params[idx+1], params[idx], err)
+				fmt.Fprintf(os.Stderr, "Cannot stat `%s' file: %s\n", config, err)
 				os.Exit(1)
 			}
 
-			attrsValues[attrId] = value
-		}
+			if (info.Mode() & 06) != 0 {
+				fmt.Fprintf(os.Stderr,
+					"`%s' file readdable and/or writtable by others. Abort!\n", config)
+				os.Exit(1)
+			}
 
-		v, pDevice := mustInitVitotrol(login, password, verbose, debug)
-
-		// Set them all
-		for attrId, value := range attrsValues {
-			ch, err := pDevice.WriteDataWait(v, attrId, value)
+			rd := bufio.NewReader(file)
+			confLogin, _ := rd.ReadString('\n')
+			confPassword, err := rd.ReadString('\n')
+			file.Close()
 			if err != nil {
-				fmt.Fprintln(os.Stderr, "WriteData error:", err)
+				fmt.Fprintf(os.Stderr, "Invalid config file `%s' contents, must contain "+
+					"login and password on two separate lines\n", config)
 				os.Exit(1)
 			}
 
-			if err = <-ch; err != nil {
-				fmt.Fprintln(os.Stderr, "WriteData failed:", err)
-				os.Exit(1)
+			if options.login == "" {
+				options.login = confLogin[:len(confLogin)-1]
 			}
-
-			if verbose {
-				fmt.Printf("%s attribute successfully set to `%s'\n",
-					vitotrol.AttributesRef[attrId].Name, value)
+			if options.password == "" {
+				options.password = confPassword[:len(confPassword)-1]
 			}
 		}
-
-	case "errors":
-		v, pDevice := mustInitVitotrol(login, password, verbose, debug)
-
-		err := pDevice.GetErrorHistory(v)
-		if err != nil {
-			fmt.Fprintln(os.Stderr, "GetErrorHistory error:", err)
-			os.Exit(1)
-		}
-
-		if len(pDevice.Errors) == 0 {
-			fmt.Println("No errors")
-		} else {
-			fmt.Printf("%d error(s):\n", len(pDevice.Errors))
-			for _, error := range pDevice.Errors {
-				fmt.Println("-", &error)
-			}
-		}
-
-	case "set_timesheet":
-		tId := mustExistTimesheetName(params[0])
-		if len(params) == 1 {
-			fmt.Fprintln(os.Stderr, "*** JSON definition of timesheet is missing!")
-			flag.Usage()
-			os.Exit(1)
-		}
-		tss := make(map[string]vitotrol.TimeslotSlice)
-		var data []byte
-		if strings.HasPrefix(params[1], "@") && len(params[1]) > 1 {
-			var err error
-			data, err = ioutil.ReadFile(params[1][1:])
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "Cannot read file %s: %s\n", params[1][1:], err)
-				os.Exit(1)
-			}
-		} else {
-			data = []byte(params[1])
-		}
-		err := json.Unmarshal(data, &tss)
-		if err != nil {
-			fmt.Fprintln(os.Stderr,
-				"*** JSON definition of timesheet is invalid:", err)
-			os.Exit(1)
-		}
-
-		v, pDevice := mustInitVitotrol(login, password, verbose, debug)
-
-		ch, err := pDevice.WriteTimesheetDataWait(v, tId, tss)
-		if err != nil {
-			fmt.Fprintln(os.Stderr, "WriteTimesheetData error:", err)
-			os.Exit(1)
-		}
-
-		if err = <-ch; err != nil {
-			fmt.Fprintln(os.Stderr, "WriteTimesheetData failed:", err)
-			os.Exit(1)
-		}
-
-	case "timesheet":
-		timesheetIds := make([]vitotrol.TimesheetId, len(params))
-		for idx, name := range params {
-			timesheetIds[idx] = mustExistTimesheetName(name)
-		}
-
-		v, pDevice := mustInitVitotrol(login, password, verbose, debug)
-
-		for _, tId := range timesheetIds {
-			err := pDevice.GetTimesheetData(v, tId)
-			if err != nil {
-				fmt.Fprintln(os.Stderr, "GetTimesheetData error:", err)
-				os.Exit(1)
-			}
-
-			ts := pDevice.Timesheets[tId]
-			if jsonOutput {
-				buf, _ := json.Marshal(ts)
-				fmt.Println(string(buf))
-			} else {
-				fmt.Println(vitotrol.TimesheetsRef[tId])
-				for _, day := range []string{"mon", "tue", "wed", "thu", "fri", "sat", "sun"} {
-					fmt.Printf("- %s:\n", day)
-					for _, slot := range ts[day] {
-						fmt.Printf("  %s\n", &slot)
-					}
-				}
-			}
-		}
-
-	default:
-		panic("Unhandled action `" + action + "'")
-	}
-}
-
-func mustInitVitotrol(login, password string, verbose, debug bool) (*vitotrol.Session, *vitotrol.Device) {
-	v, pDevice, err := initVitotrol(login, password, verbose, debug)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "Fatal error:", err)
-		os.Exit(1)
-	}
-	return v, pDevice
-}
-
-func initVitotrol(login, password string, verbose, debug bool) (*vitotrol.Session, *vitotrol.Device, error) {
-	v := &vitotrol.Session{
-		Debug: debug,
 	}
 
-	err := v.Login(login, password)
-	if err != nil {
-		return nil, nil, fmt.Errorf("Login failed: %s", err)
-	}
-
-	err = v.GetDevices()
-	if err != nil {
-		return nil, nil, fmt.Errorf("GetDevices failed: %s", err)
-	}
-	if len(v.Devices) == 0 {
-		return nil, nil, errors.New("No device found!")
-	}
-
-	pDevice := &v.Devices[0]
-	if !pDevice.IsConnected {
-		return nil, nil, fmt.Errorf("Device %s@%s is not connected",
-			pDevice.DeviceName, pDevice.LocationName)
-	}
-
-	if verbose {
-		fmt.Printf("Working with device %s@%s\n",
-			pDevice.DeviceName, pDevice.LocationName)
-	}
-
-	return v, pDevice, nil
-}
-
-func checkAttributeAccess(attrName string, reqAccess vitotrol.AttrAccess) (vitotrol.AttrId, error) {
-	attrId, ok := vitotrol.AttributesNames2Ids[attrName]
-	if !ok {
-		return vitotrol.NoAttr, fmt.Errorf("unknown attribute `%s'", attrName)
-	}
-
-	if (vitotrol.AttributesRef[attrId].Access & reqAccess) != reqAccess {
-		return vitotrol.NoAttr, fmt.Errorf("attribute `%s' is not %s",
-			attrName, vitotrol.AccessToStr[reqAccess])
-	}
-
-	return attrId, nil
-}
-
-func mustCheckAttributeAccess(attrName string, reqAccess vitotrol.AttrAccess) vitotrol.AttrId {
-	attrId, err := checkAttributeAccess(attrName, reqAccess)
+	err = action.Do(&options, params)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "***", err)
-		flag.Usage()
 		os.Exit(1)
 	}
-	return attrId
-}
-
-func mustExistTimesheetName(tsName string) vitotrol.TimesheetId {
-	tId, ok := vitotrol.TimesheetsNames2Ids[tsName]
-	if !ok {
-		fmt.Fprintf(os.Stderr, "*** unknown timesheet `%s'\n", tsName)
-		flag.Usage()
-		os.Exit(1)
-	}
-	return tId
 }
